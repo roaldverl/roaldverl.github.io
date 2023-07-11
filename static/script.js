89633bb23b1f0c334520c487a7925241e91d9018
roaldverl <100352633+roaldverl@users.noreply.github.com>
2023-07-11 10:11:42 +0200

    updates

--- static/script.js
+++ static/script.js
@@ -1,12 +1,17 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mainContent").style.marginLeft = "250px";
-    document.getElementById("header").style.marginLeft = "250px";
-    document.getElementById("header-menu-button").style.display = "none";
-    /*document.getElementById("header").classList.add("header-overlay");*/
-    /*document.body.style.backgroundColor = "rgba(0,0,0,0.4)";*/
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+  if (windowWidth < 768) { // Adjust the screen width breakpoint as needed
+      document.getElementById("mySidenav").style.width = "100%";
+      document.getElementById("mainContent").style.marginTop = "250px"; // Add top margin to create space for the menu
+  } else {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("mainContent").style.marginLeft = "250px";
+      document.getElementById("header").style.marginLeft = "250px";
+      document.getElementById("header-menu-button").style.display = "none";
   }
+}
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
